2753e132a41a345b7f55e01e0b1c0585847147e9
Upendra Sharma <upendra861@gmail.com>
2018-07-02 17:15:46 +0530

    Index Page Added

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("a1").onclick = function () 
+{
+	location.href = "Time-Table/TimeTable.html";
+};
+
+document.getElementById("a2").onclick = function () 
+{
+	location.href = "Todo-List/Todo.html";
+};
+
+document.getElementById("a3").onclick = function () 
+{
+	location.href = "Profile/main.html";
+};
